66b0ff1cd8603fff3c39606c145a1246ec28feed
Federico "EvilScript" Torrielli <evilscript@protonmail.com>
2022-01-21 18:52:29 +0100

    Various refactors + Pizza changes + Removed useless commands + The program is not extremely racist (jk, lol)

--- pywordlesolver.py
+++ pywordlesolver.py
@@ -2,7 +2,8 @@ from pprint import pprint
 from random import random
 
 result_list = set()
-most_probable_letters_in_5wordenglish = ['e','a','r','i','o','t','n','s']
+most_probable_letters_in_5wordenglish = ["e", "a", "r", "i", "o", "t", "n", "s"]
+
 
 def prepare_word_set():
     # put the words in "5words.txt" into the set "word_set"
@@ -10,11 +11,33 @@ def prepare_word_set():
         for line in f:
             result_list.add(line.strip().lower())
 
+
 def choose_starting_word():
     global result_list
     # return a random word from the list "starting_list"
-    starting_list = ["frame","graze","windy","paint","gourd","swing","vapes","audio","farts","adieu","ouija","ready","pears","chief","touch","arise","roast","tears","meats","pizza"]
+    starting_list = [
+        "frame",
+        "graze",
+        "windy",
+        "paint",
+        "gourd",
+        "swing",
+        "vapes",
+        "audio",
+        "farts",
+        "adieu",
+        "ouija",
+        "ready",
+        "pears",
+        "chief",
+        "touch",
+        "arise",
+        "roast",
+        "tears",
+        "meats",
+    ]
     return starting_list[int(len(starting_list) * random())]
+
 
 def choose_most_probable_word():
     global result_list
@@ -27,48 +50,27 @@ def choose_most_probable_word():
         max_diversity = 0
         max_diversity_word_list = []
         for word in result_list:
-            diversity = 0
-            for letter in word:
-                if letter not in max_diversity_word:
-                    diversity += 1
+            word_letters = "".join(set(word))
+            diversity = len(word_letters)
             if diversity > max_diversity:
                 max_diversity = diversity
                 max_diversity_word_list = []
                 max_diversity_word_list.append(word)
-            else if diversity = max_diversity:
+            elif diversity == max_diversity:
                 max_diversity_word_list.append(word)
-        
+
         number_of_frequent_letters = 0
         best_word = ""
         for word in max_diversity_word_list:
             count = 0
             for letter in word:
-               if letter in most_probable_letters_in_5wordenglish:
+                if letter in most_probable_letters_in_5wordenglish:
                     count += 1
             if count > number_of_frequent_letters:
                 number_of_frequent_letters = count
                 best_word = word
-            
+
         return best_word
-
-def remove_word_that_doesnt_start_with(letter):
-    global result_list
-    temp_set = set()
-    # Remove the words that doesn't start with "letter" from the list "result_list"
-    for word in result_list:
-        if not word.startswith(letter):
-            temp_set.add(word)
-    result_list = result_list - temp_set
-
-
-def remove_word_that_doesnt_ends_with(letter):
-    global result_list
-    temp_set = set()
-    # Remove the words that doesn't end with "letter" from the list "result_list"
-    for word in result_list:
-        if not word.endswith(letter):
-            temp_set.add(word)
-    result_list = result_list - temp_set
 
 
 def remove_word_that_not_contains(letter):
@@ -101,42 +103,69 @@ def remove_word_that_hasnt_letter_in_pos(letter, pos):
     result_list = result_list - temp_set
 
 
+def remove_word_that_has_letter_in_pos(letter, pos):
+    global result_list
+    temp_set = set()
+    # Remove the words that has letter in pos from the list "result_list"
+    for word in result_list:
+        if letter in word[int(pos) - 1]:
+            temp_set.add(word)
+    result_list = result_list - temp_set
+
+
+def process_input(input_string, letter=True, position=False):
+    """
+    Process the input string and verify that if letter==True then it must be a character, not a number
+    while if it is a position, it must be a number, not a character. In neither cases, it must not be
+    a special character.
+    """
+    if not input_string.isalnum():
+        raise ValueError("The input must be a character or a number")
+    if letter:
+        if not input_string.isalpha():
+            raise ValueError("The input must be a character")
+    elif position:
+        if not input_string.isdigit():
+            raise ValueError("The input must be a number")
+    return input_string
+
+
 def main():
     global result_list
     print(f"Most probable starting word: {choose_starting_word()}")
     while True:
-        # Ask with the user what he/she wants to do
         print(
-            """
-        1. Remove words that does not start with a letter
-        2. Remove words that does not end with a letter
-        3. Remove words that contains a letter
-        4. Remove words that does not contain a letter
-        5. Remove words that has NOT a letter in a certain position
-        6. Gimme another word!
-        7. Exit
+        """
+        1. Remove words that contains a letter
+        2. Remove words that does NOT contain a letter
+        3. Remove words that has NOT a letter in a certain position
+        4. Remove words that has a letter in a certain position
+        5. Gimme another word!
+        6. Exit
         """
         )
         choice = input("Enter your choice: ")
-        if choice == "7":
+        if choice == "6":
             break
         elif choice == "1":
-            letter = input("Enter the letter: ")
-            remove_word_that_doesnt_start_with(letter)
-        elif choice == "2":
-            letter = input("Enter the letter: ")
-            remove_word_that_doesnt_ends_with(letter)
-        elif choice == "3":
-            letter = input("Enter the letter: ")
+            letter = process_input(input("Enter the letter: "))
             remove_word_that_contains(letter)
-        elif choice == "4":
-            letter = input("Enter the letter: ")
+        elif choice == "2":
+            letter = process_input(input("Enter the letter: "))
             remove_word_that_not_contains(letter)
-        elif choice == "5":
-            letter = input("Enter the letter: ")
-            pos = input("Enter the position: ")
+        elif choice == "3":
+            letter = process_input(input("Enter the letter: "))
+            pos = process_input(
+                input("Enter the position: "), letter=False, position=True
+            )
             remove_word_that_hasnt_letter_in_pos(letter, pos)
-        elif choice == "6":
+        elif choice == "4":
+            letter = process_input(input("Enter the letter: "))
+            pos = process_input(
+                input("Enter the position: "), letter=False, position=True
+            )
+            remove_word_that_has_letter_in_pos(letter, pos)
+        elif choice == "5":
             result_list.remove(word)
             word = choose_most_probable_word()
             print(f"Most probable word: {word}")
