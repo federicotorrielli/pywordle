87d0071fd782b2fdf43181dffd193c4e29d2d118
Federico "EvilScript" Torrielli <evilscript@protonmail.com>
2022-01-27 11:31:43 +0100

    Small refactoring of code - pt2

--- pywordlesolver.py
+++ pywordlesolver.py
@@ -1,16 +1,16 @@
 from math import log
 
-frequencies = {"a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, "i": 0, "j": 0, "k": 0, "l": 0, "m": 0, 
-                    "n": 0, "o": 0, "p": 0, "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0, "y": 0, "z": 0}
-                    
-emp_probabilities = {"a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, "i": 0, "j": 0, "k": 0, "l": 0, "m": 0, 
-                    "n": 0, "o": 0, "p": 0, "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0, "y": 0, "z": 0}
-                    
+frequencies = {"a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, "i": 0, "j": 0, "k": 0, "l": 0, "m": 0,
+               "n": 0, "o": 0, "p": 0, "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0, "y": 0, "z": 0}
+
+emp_probabilities = frequencies
+
+
 class WordleSolver:
     def __init__(self):
         self.word_set = set()
         self.good_letters = set()  # Letters that are in the word and should not be removed
-        self.proved_letters = set() # Letters used in previous guess
+        self.proved_letters = set()  # Letters used in previous guess
         self.most_probable_word = ""
         self.prepare_word_set()
         self.original_cardinality = len(self.word_set)
@@ -28,16 +28,16 @@ class WordleSolver:
             freq = 0
             for word in self.word_set:
                 freq = freq + word.count(k)
-            frequencies[k] = freq/(len(self.word_set)*5)
-    	
+            frequencies[k] = freq / (len(self.word_set) * 5)
+
     def set_empprobabilities(self):
         for k in emp_probabilities.keys():
             count = 0
             for word in self.word_set:
                 if k in word:
-                	count = count + 1
+                    count = count + 1
             # define emp probability with m-estimate of frequency of that letter
-            emp_probabilities[k] = (count + (frequencies[k]*len(self.word_set)))/(2*len(self.word_set))
+            emp_probabilities[k] = (count + (frequencies[k] * len(self.word_set))) / (2 * len(self.word_set))
 
     def choose_most_probable_word(self):
         if len(self.word_set) == 0:
@@ -61,7 +61,7 @@ class WordleSolver:
                     max_diversity_word_list.append(word)
 
             best_word = ""
-            best_score = float('-inf')        
+            best_score = float('-inf')
 
             for word in max_diversity_word_list:
                 word_score = 0
@@ -73,7 +73,7 @@ class WordleSolver:
                 if word_score > best_score:
                     best_score = word_score
                     best_word = word
-            
+
             self.most_probable_word = best_word
 
     def remove_word_that_not_contains(self, letter):
