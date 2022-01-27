18c05b19cc3d84bc534de98469f9710453ad6ce8
Lorenzo Sciandra <49808768+LorenzoSciandra@users.noreply.github.com>
2022-01-27 10:28:27 +0100

    Bayes Optimality
    Scelta della parola con numero di lettere diverse maggiore più probabile in accordo con il nostro dataset. La parola è quella che ci permetterà di ridurre maggiormente la dimensione del dataset nel caso fosse sbagliata. Ovviamente ho presupposto, per facilità implentativa, condizioni di indipendenza tra lettere nella stessa parola

--- pywordlesolver.py
+++ pywordlesolver.py
@@ -1,10 +1,13 @@
-most_probable_letters_in_dataset = ["e", "a", "r", "i", "o", "t", "n", "s"]
+from math import log
 
-
+frequencies = {"a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, "i": 0, "j": 0, "k": 0, "l": 0, "m": 0, 
+                    "n": 0, "o": 0, "p": 0, "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0, "y": 0, "z": 0}
+                    
 class WordleSolver:
     def __init__(self):
         self.word_set = set()
         self.good_letters = set()  # Letters that are in the word and should not be removed
+        self.proved_letters = set() # Letters used in previous guess
         self.most_probable_word = ""
         self.prepare_word_set()
         self.original_cardinality = len(self.word_set)
@@ -17,35 +20,47 @@ class WordleSolver:
             for line in f:
                 self.word_set.add(line.strip().lower())
 
+    def set_dictionary(self):
+        for k in frequencies.keys():
+            freq = 1 #Laplace Correction
+            for word in self.word_set:
+                freq = freq + word.count(k)
+            frequencies[k] = freq/((len(self.word_set)*5)+26)
+
     def choose_most_probable_word(self):
         if len(self.word_set) == 0:
             return "There is no word that meets the requirements"
         else:
+            self.set_dictionary()
             max_diversity = 0
             max_diversity_word_list = []
             for word in self.word_set:
                 word_letters = "".join(set(word))
-                diversity = len(word_letters)
+                diversity = 0
+                for letter in word_letters:
+                    if letter not in self.proved_letters:
+                        diversity = diversity + 1
+
                 if diversity > max_diversity:
                     max_diversity = diversity
                     max_diversity_word_list = [word]
                 elif diversity == max_diversity:
                     max_diversity_word_list.append(word)
 
-            number_of_frequent_letters = 0
             best_word = ""
+            best_score = float('-inf')        
+
             for word in max_diversity_word_list:
-                count = 0
-                for letter in word:
-                    if letter in most_probable_letters_in_dataset:
-                        count += 1
-                if count > number_of_frequent_letters:
-                    number_of_frequent_letters = count
+                word_score = 0
+                different_letters = "".join(set(word))
+                for letter in different_letters:
+                    if letter not in self.proved_letters:
+                        emp_probability = frequencies.get(letter)
+                        word_score = word_score + log(emp_probability)
+                if word_score > best_score:
+                    best_score = word_score
                     best_word = word
-
-            if len(best_word) == 0:
-                best_word = max_diversity_word_list[0]
-
+            
             self.most_probable_word = best_word
 
     def remove_word_that_not_contains(self, letter):
@@ -82,16 +97,23 @@ class WordleSolver:
 
     def solve(self, letters: list, colors: list):
         for letter in letters:
+
+            if letter not in self.proved_letters:
+                self.proved_letters.add(letter)
+
             if colors[letters.index(letter)] == "Green":
-                self.good_letters.add(letter)
                 self.remove_word_that_not_contains(letter)
                 self.remove_word_that_hasnt_letter_in_pos(letter, pos=letters.index(letter))
+                if letter not in self.good_letters:
+                    self.good_letters.add(letter)
             elif colors[letters.index(letter)] == "Grey":
                 if letter not in self.good_letters:
                     self.remove_word_that_contains(letter)
             else:
                 self.remove_word_that_not_contains(letter)
                 self.remove_word_that_has_letter_in_pos(letter, pos=letters.index(letter))
+                if letter not in self.good_letters:
+                    self.good_letters.add(letter)
         self.cardinality = abs(self.cardinality - len(self.word_set))
         self.choose_most_probable_word()
 
